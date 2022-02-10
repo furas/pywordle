f81e48ccde28babfdb42fd622fe7376e618e8693
furas <furas@tlen.pl>
2022-02-10 18:41:12 +0100

    update tkinter version

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 Wordle in Python using different GUIs/modules.
 
-At this moment only with tkinter but later I will try to do the same with PyQt, wxPython, Kivy, and maybe web version with Flask.
+At this moment only with tkinter but still not ready.
+
+Later I will try to do the same with PyQt, wxPython, Kivy, and maybe as web version with Flask, Bottle or Django.
+
--- wordle-in-tkinter/main.py
+++ wordle-in-tkinter/main.py
@@ -1,9 +1,13 @@
+# author: Bartlomiej "furas" Burek (https://blog.furas.pl)
+# date: 2022.02.10
+
 import tkinter as tk
 import random
 
 # --- constants ---
 
 WORD_LENGTH = 5
+MAX_WORDS = 6
 
 COLOR_GREY   = '#ABABAB'
 COLOR_GREEN  = '#9BDB4D'
@@ -18,51 +22,112 @@ def debug(*args, **kwargs):
         print('[debug]', *args, **kwargs)
     
 def check_word(word):
-    return False
+    result = []
+    status = True
+    debug('[check_word] word:', word)
+    for n, char in enumerate(word):
+        debug('[check_word] ', n, char, selected_word[n])
+        if char == selected_word[n]:
+            result.append(COLOR_GREEN)
+        elif char in selected_word:
+            result.append(COLOR_YELLOW)
+            status = False
+        else:
+            result.append(COLOR_GREY)
+            status = False
+        
+    return status, result
 
+def reset():
+    global selected_word
+    global current_row
+    global current_col
+    global current_word
+
+    selected_word = random.choice(words).upper()
+
+    current_row = 0
+    current_col = 0
+    current_word = ''
+
+    for key in all_buttons.values():
+        key['bg'] = None
+
+    for word_labels in all_labels:
+        for label in word_labels:
+            label['text'] = ''
+            label['bg'] = 'white'
+        
 def on_click(key):
     global current_row
     global current_col
     global current_word
     
-    debug('key:', key)
+    debug('[on_click] key:', key)
+
+    label_message['text'] = ''
+
+    if current_row >= MAX_WORDS:
+        label_message['text'] = 'Press ENTER to start'
+        
+        return
     
     if key == 'ENTER':
+        if current_row >= MAX_WORDS:
+            reset()
+            return
+        
         #if current_col < WORD_LENGTH:
         if len(current_word) < WORD_LENGTH:
-            print('too short')
+            label_message['text'] = 'too short'
             return
             
-        if check_word(current_word):
-            for c in range(5):
-                all_labels[current_row][c]['bg'] = COLOR_GREY
-                char = all_labels[current_row][c]['text']
-                all_buttons[char]['bg'] = '#ABABAB'
+        status, result = check_word(current_word)
+        debug('[on_click]:', status, result)
+        
+        for c, (char, color) in enumerate(zip(current_word, result)):
+            all_labels[current_row][c]['bg'] = color
+            char = all_labels[current_row][c]['text']
+            if all_buttons[char]['bg'] != COLOR_GREEN:
+                all_buttons[char]['bg'] = color
+                
+        if not status:
             # move to next line
             current_row += 1
             current_col = 0
+            current_word = ''
         else:
             pass
+        
+        if current_row >= MAX_WORDS:
+            label_message['text'] = f'correct: {selected_word}'
             
     elif key == 'BACK':
         if current_col > 0:
-            current_col -= 1
             current_word = current_word[:-1]
+            current_col -= 1
             all_labels[current_row][current_col]['text'] = ''
     else:
         if current_col < WORD_LENGTH:
+            current_word += key
             all_labels[current_row][current_col]['text'] = key
             current_col += 1
         else:
-            print('too long')
+            label_message['text'] = 'too long'
 
 # --- main ---
 
-selected_word = 'banan'.upper()
+words = ['banan', 'canon', 'kajak']
+
+# - (re)set data -
+
+selected_word = random.choice(words).upper()
 
 current_row = 0
 current_col = 0
 current_word = ''
+
+#reset()
 
 # -  -
 
@@ -80,7 +145,7 @@ frame_words.pack()
 
 all_labels = []
 
-for r in range(1, 1+6):
+for r in range(MAX_WORDS):
     word_labels = []
     all_labels.append(word_labels)
     for c in range(WORD_LENGTH):
@@ -93,6 +158,11 @@ for r in range(1, 1+6):
                          font=(None, 16, 'bold'))
         label.grid(row=r, column=c, padx=3, pady=3)
         word_labels.append(label)
+
+# - GUI message -
+
+label_message = tk.Label(root, pady=8, font=(None, 16, 'bold'))
+label_message.pack()
         
 # - GUI keyboard -
 
